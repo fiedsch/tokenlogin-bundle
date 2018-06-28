ea160e27840b3934ec8497bd1dded109cd3c01ee
Andreas Fieger <fiedsch@ja-eh.at>
2018-06-28 06:58:31 +0200

    remove routing

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -13,11 +13,8 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
@@ -28,16 +25,6 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
             BundleConfig::create(FiedschTokenloginBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-        return $resolver
-            ->resolve(__DIR__ . '/../Resources/config/routing.yml')
-            ->load(__DIR__ . '/../Resources/config/routing.yml');
     }
 
 }
