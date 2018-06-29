7bd74be9235f7010d014f60bd1ebe44f21a8b7e5
Andreas Fieger <fiedsch@ja-eh.at>
2018-06-29 19:14:13 +0200

    closes #1

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,6 +2,5 @@
 
 $GLOBALS['FE_MOD']['user']['tokenlogin'] = 'Fiedsch\TokenloginBundle\ModuleTokenlogin';
 
-$GLOBALS['TL_HOOKS']['importUser'][] = array('Fiedsch\TokenloginBundle\MyHooks', 'myImportUser');
 
 
